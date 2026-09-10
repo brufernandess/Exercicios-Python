be99c5135d76c6174d00a40d60daeec0de8c0bc4
brufernandess <brunaap.fernandess@gmail.com>
2026-09-10 14:49:00 -0300

    Atualizando Exercicio 03

--- Exercicio 03_Conversao_De_Bases.py
+++ Exercicio 03_Conversao_De_Bases.py
@@ -1,1 +1,15 @@
 # Exercício 03 - Binário, Octal e Hexadecimal
+numero=int(input('Digite um numero: '))
+
+
+
+n1=int(input('Digite 1 para BINARIO, Digite 2 para OCTAL e 3 para HEXADECIMAL: '))
+
+
+
+if n1 == 1:
+    print(f'Voce escolheu a opção BINARIO, seu numero fica: {bin(numero)}')
+elif n1 == 2:
+    print(f'Voce escolheu a opção OCTAL, seu numero fica: {oct(numero)}')
+elif n1 == 3:
+    print(f'Voce escolheu a opção HEXADECIMAL, seu numero fica: {hex(numero)}')
